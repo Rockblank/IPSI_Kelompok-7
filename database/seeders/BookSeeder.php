9b3e32dab78d4b1d89c5664e466e22e1ddf2a9c1
hikarl1 <aureliacallysta44@gmail.com>
2026-05-21 18:10:56 +0700

    [dashboard & pencarian]: finishing dashboard & pencarian

--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -2,16 +2,49 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Book;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents; 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; // 1. PASTIKAN IMPORT INI ADA
 
 class BookSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+    use WithoutModelEvents;
+
     public function run(): void
     {
-        //
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        
+        Book::truncate();
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
+        $books = [
+            [
+                'book_title' => 'Laut Bercerita',
+                'author' => 'Leila S. Chudori',
+                'publisher' => 'Kepustakaan Populer Gramedia',
+                'available_stock' => 2,
+                'book_status' => 'available'
+            ],
+            [
+                'book_title' => 'Kisah Kapal Karam Laut Jawa',
+                'author' => 'Aditya Nugroho',
+                'publisher' => 'UB Press',
+                'available_stock' => 1,
+                'book_status' => 'available'
+            ],
+            [
+                'book_title' => 'Meniti Ombak Selat Sunda',
+                'author' => 'Siti Aminah',
+                'publisher' => 'Erlangga',
+                'available_stock' => 0,
+                'book_status' => 'not_available'
+            ]
+        ];
+
+        foreach ($books as $book) {
+            Book::create($book);
+        }
     }
 }
